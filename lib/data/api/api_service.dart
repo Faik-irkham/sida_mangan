684c8b39acad0fe6dbd7f9b648012574bc3be1fc
faik-irkham <faikirkham@gmail.com>
2023-12-25 03:02:07 +0700

    add model search restaurant dan add service

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:sida_mangan/common/constant.dart';
 import 'package:sida_mangan/data/model/detail_restaurant.dart';
 import 'package:sida_mangan/data/model/restaurant.dart';
+import 'package:sida_mangan/data/model/search_restaurant_model.dart';
 
 class ApiService {
   Future<RestaurantsModel> listRestaurant() async {
@@ -21,6 +22,17 @@ class ApiService {
       return DetailRestaurantModel.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to load detail restaurant');
+    }
+  }
+
+  Future<SearchRestaurantModel> searchReataurant() async {
+    final response = await http.get(Uri.parse('$baseUrl/search?q=<query>'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return SearchRestaurantModel.fromJson(data);
+    } else {
+      throw Exception('Failed search restaurant');
     }
   }
 }
---
+++ lib/data/model/search_restaurant_model.dart
@@ -0,0 +1,78 @@
+// To parse this JSON data, do
+//
+//     final searchRestaurantModel = searchRestaurantModelFromJson(jsonString);
+
+class SearchRestaurantModel {
+  final bool error;
+  final int founded;
+  final List<Restaurant> restaurants;
+
+  SearchRestaurantModel({
+    required this.error,
+    required this.founded,
+    required this.restaurants,
+  });
+
+  SearchRestaurantModel copyWith({
+    bool? error,
+    int? founded,
+    List<Restaurant>? restaurants,
+  }) =>
+      SearchRestaurantModel(
+        error: error ?? this.error,
+        founded: founded ?? this.founded,
+        restaurants: restaurants ?? this.restaurants,
+      );
+
+  factory SearchRestaurantModel.fromJson(Map<String, dynamic> json) =>
+      SearchRestaurantModel(
+        error: json["error"],
+        founded: json["founded"],
+        restaurants: List<Restaurant>.from(
+            json["restaurants"].map((x) => Restaurant.fromJson(x))),
+      );
+}
+
+class Restaurant {
+  final String id;
+  final String name;
+  final String description;
+  final String pictureId;
+  final String city;
+  final double rating;
+
+  Restaurant({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.pictureId,
+    required this.city,
+    required this.rating,
+  });
+
+  Restaurant copyWith({
+    String? id,
+    String? name,
+    String? description,
+    String? pictureId,
+    String? city,
+    double? rating,
+  }) =>
+      Restaurant(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        description: description ?? this.description,
+        pictureId: pictureId ?? this.pictureId,
+        city: city ?? this.city,
+        rating: rating ?? this.rating,
+      );
+
+  factory Restaurant.fromJson(Map<String, dynamic> json) => Restaurant(
+        id: json["id"],
+        name: json["name"],
+        description: json["description"],
+        pictureId: json["pictureId"],
+        city: json["city"],
+        rating: json["rating"]?.toDouble(),
+      );
+}
