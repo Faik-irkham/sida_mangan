f4e5af95bb4c0a56d6feae47a8b5fabb00c92456
faik-irkham <faikirkham@gmail.com>
2023-12-30 00:20:51 +0700

    clean code restaurant

--- lib/data/model/search_restaurant_model.dart
+++ lib/data/model/search_restaurant_model.dart
@@ -1,3 +1,5 @@
+import 'package:sida_mangan/data/model/restaurant_model.dart';
+
 class SearchRestaurantModel {
   final bool error;
   final int founded;
@@ -32,58 +34,5 @@ class SearchRestaurantModel {
         "error": error,
         "founded": founded,
         "restaurants": List<dynamic>.from(restaurants.map((x) => x.toJson())),
-      };
-}
-
-class Restaurant {
-  final String id;
-  final String name;
-  final String description;
-  final String pictureId;
-  final String city;
-  final double rating;
-
-  Restaurant({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.pictureId,
-    required this.city,
-    required this.rating,
-  });
-
-  Restaurant copyWith({
-    String? id,
-    String? name,
-    String? description,
-    String? pictureId,
-    String? city,
-    double? rating,
-  }) =>
-      Restaurant(
-        id: id ?? this.id,
-        name: name ?? this.name,
-        description: description ?? this.description,
-        pictureId: pictureId ?? this.pictureId,
-        city: city ?? this.city,
-        rating: rating ?? this.rating,
-      );
-
-  factory Restaurant.fromJson(Map<String, dynamic> json) => Restaurant(
-        id: json["id"],
-        name: json["name"],
-        description: json["description"],
-        pictureId: json["pictureId"],
-        city: json["city"],
-        rating: json["rating"]?.toDouble(),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-        "description": description,
-        "pictureId": pictureId,
-        "city": city,
-        "rating": rating,
       };
 }
