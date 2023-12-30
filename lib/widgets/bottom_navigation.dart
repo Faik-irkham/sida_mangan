824c24c3e027cbdbefe2e7ccffc53a4e3f16c87e
faik-irkham <faikirkham@gmail.com>
2023-12-31 04:27:39 +0700

    edit bottom navigation bar

--- lib/widgets/bottom_navigation.dart
+++ lib/widgets/bottom_navigation.dart
@@ -4,25 +4,35 @@ class BottomNavBar extends StatelessWidget {
   final int currentIndex;
   final ValueChanged<int> onTap;
 
-  const BottomNavBar({super.key, required this.currentIndex, required this.onTap});
+  const BottomNavBar(
+      {super.key, required this.currentIndex, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: currentIndex,
       onTap: onTap,
+      selectedItemColor: Colors.black,
+      iconSize: 24,
+      type: BottomNavigationBarType.fixed,
       items: const [
         BottomNavigationBarItem(
-          icon: Icon(Icons.home),
+          icon: Icon(
+            Icons.home,
+          ),
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.details),
-          label: 'Detail',
+          icon: Icon(
+            Icons.favorite,
+          ),
+          label: 'Favorite',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.details),
-          label: 'Detail',
+          icon: Icon(
+            Icons.settings_suggest,
+          ),
+          label: 'Setting',
         ),
       ],
     );
