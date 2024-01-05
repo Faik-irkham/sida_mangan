1c729b9b3013735dbe56f860879820cb199cce3f
faik-irkham <faikirkham@gmail.com>
2024-01-06 05:44:57 +0700

    edit main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
+import 'package:sida_mangan/common/navigation.dart';
 import 'package:sida_mangan/common/style.dart';
 import 'package:sida_mangan/data/api/api_service.dart';
 import 'package:sida_mangan/data/db/database_helper.dart';
-import 'package:sida_mangan/provider/app_navigator.dart';
 import 'package:sida_mangan/provider/database_provider.dart';
 import 'package:sida_mangan/provider/restaurants_provider.dart';
 import 'package:sida_mangan/provider/review_restaurant_provider.dart';
 import 'package:sida_mangan/provider/search_restaurant_provider.dart';
+import 'package:sida_mangan/ui/detail_restaurant_page.dart';
+import 'package:sida_mangan/ui/home_page.dart';
 import 'package:sida_mangan/ui/onboarding_screen.dart';
+import 'package:sida_mangan/utils/background_service.dart';
+import 'package:sida_mangan/utils/notification_helper.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('id_ID', null);
-  
+
+  final NotificationHelper notificationHelper = NotificationHelper();
+  final BackgroundService service = BackgroundService();
+
+  service.initializeIsolate();
+
+  await notificationHelper.initNotfications(flutterLocalNotificationsPlugin);
+
   runApp(
     MultiProvider(
       providers: [
+        ChangeNotifierProvider(
+          create: (context) => RestaurantsProvider(apiService: ApiService()),
+        ),
         ChangeNotifierProvider(
           create: (context) => ReviewProviders(),
         ),
         ChangeNotifierProvider(
           create: (context) => SearchRestaurantsProvider(),
         ),
-        ChangeNotifierProvider(
-          create: (context) => RestaurantsProvider(apiService: ApiService()),
-        ),
-        ChangeNotifierProvider(create: (context) => AppNavigator()),
         ChangeNotifierProvider(
           create: (_) => DatabaseProvider(
             databaseHelper: DatabaseHelper(),
@@ -48,6 +59,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Restaurant',
+      navigatorKey: navigatorKey,
       theme: ThemeData(
         textTheme: myTextTheme,
         elevatedButtonTheme: ElevatedButtonThemeData(
@@ -64,9 +76,14 @@ class MyApp extends StatelessWidget {
             fontSize: 16,
           ),
         ),
-        
       ),
       home: const OnBoardingScreen(),
+      routes: {
+        HomePage.routeName: (context) => const HomePage(),
+        DetailRestaurant.routeName: (context) => DetailRestaurant(
+              id: ModalRoute.of(context)?.settings.arguments as String,
+            )
+      },
     );
   }
 }
