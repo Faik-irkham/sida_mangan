3ef31f5c7b86a41b95987a5a1c3a371dd923f174
faik-irkham <faikirkham@gmail.com>
2024-01-13 11:14:51 +0700

    edit schedulling provider

--- lib/provider/scheduling_provider.dart
+++ lib/provider/scheduling_provider.dart
@@ -2,16 +2,21 @@ import 'dart:developer';
 
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:sida_mangan/data/preference/schedulling_preference.dart';
 import 'package:sida_mangan/utils/background_service.dart';
 import 'package:sida_mangan/utils/date_time_helper.dart';
 
 class SchedulingProvider extends ChangeNotifier {
+  SchedulingPref schedulingPref = SchedulingPref();
   bool _isScheduled = false;
 
   bool get isScheduled => _isScheduled;
 
   Future<bool> scheduledRecomendations(bool value) async {
     _isScheduled = value;
+    schedulingPref.setScheduling(value);
+    notifyListeners();
+
     if (_isScheduled) {
       log('Schedulling Recomendations Activated');
       notifyListeners();
