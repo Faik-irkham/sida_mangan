fbed87d553336c04490c3024bd362e8a083d60f9
faik-irkham <faikirkham@gmail.com>
2024-01-13 11:14:07 +0700

    edit notification helper

--- lib/utils/notification_helper.dart
+++ lib/utils/notification_helper.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:rxdart/subjects.dart';
 import 'package:sida_mangan/common/navigation.dart';
@@ -17,27 +18,22 @@ class NotificationHelper {
 
   factory NotificationHelper() => _instance ?? NotificationHelper._internal();
 
-  Future<void> initNotfications(
+  Future<void> initNotifications(
       FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
     var initializationSettingsAndroid =
         const AndroidInitializationSettings('app_icon');
 
-    var initializationSettingsIOS = const DarwinInitializationSettings(
-      requestAlertPermission: false,
-      requestBadgePermission: false,
-      requestSoundPermission: false,
-    );
-
-    var initializationSettings = InitializationSettings(
-        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    var initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
 
     await flutterLocalNotificationsPlugin.initialize(
       initializationSettings,
       onDidReceiveNotificationResponse: (details) {
         final payload = details.payload;
         if (payload != null) {
-          // ignore: avoid_print
-          print('notification payload: $payload');
+          if (kDebugMode) {
+            print('notification payload: $payload');
+          }
         }
         selectNotificationSubject.add(payload ?? 'empty payload');
       },
@@ -49,7 +45,7 @@ class NotificationHelper {
       RestaurantsModel restaurants) async {
     var channelId = "1";
     var channelName = "channel_01";
-    var channelDescription = "Recomendation Restaurant channel";
+    var channelDescription = "dicoding news channel";
 
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
         channelId, channelName,
@@ -59,27 +55,28 @@ class NotificationHelper {
         ticker: 'ticker',
         styleInformation: const DefaultStyleInformation(true, true));
 
-    var iOSPlatformChannelSpecifics = const DarwinNotificationDetails();
     var platformChannelSpecifics = NotificationDetails(
       android: androidPlatformChannelSpecifics,
-      iOS: iOSPlatformChannelSpecifics,
     );
 
     var titleNotification = "<b>Recomendation Restaurants</b>";
-    // var randomIndex = random.nextInt(restaurants.restaurants.length);
-    var randomRestaurant = Random().nextInt(restaurants.count);
-    var titleRestaurant = restaurants.restaurants[randomRestaurant];
+    var randomIndex = Random().nextInt(restaurants.restaurants.length);
+    var titleRestaurant = restaurants.restaurants[randomIndex].name;
 
     await flutterLocalNotificationsPlugin.show(
-        0, titleNotification, titleRestaurant.name, platformChannelSpecifics,
-        payload: json.encode(restaurants.toJson()));
+      0,
+      titleNotification,
+      titleRestaurant,
+      platformChannelSpecifics,
+      payload: json.encode(restaurants.toJson()),
+    );
   }
 
   void configureSelectNotificationSubject(String route) {
     selectNotificationSubject.stream.listen(
       (String payload) async {
         var data = RestaurantsModel.fromJson(json.decode(payload));
-        var restaurantRandom = Random().nextInt(data.count);
+        var restaurantRandom = Random().nextInt(data.restaurants.length);
         var restaurant = data.restaurants[restaurantRandom];
         Navigation.intentWithData(route, restaurant.id!);
       },
