6b26e92f15f43263f7b9ead704999c9dfae5f438
faik-irkham <faikirkham@gmail.com>
2024-01-15 10:16:15 +0700

    edit test provider

--- test/provider_test/restaurant_provider_test.dart
+++ test/provider_test/restaurant_provider_test.dart
@@ -35,22 +35,6 @@ const apiListResponse = {
   ]
 };
 
-const apiSearchResponse = {
-  "error": false,
-  "founded": 1,
-  "restaurants": [
-    {
-      "id": "dwg2wt3is19kfw1e867",
-      "name": "Drinky Squash",
-      "description":
-          "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.",
-      "pictureId": "18",
-      "city": "Surabaya",
-      "rating": 3.9
-    }
-  ]
-};
-
 List<Restaurant> testRestaurants = [
   Restaurant(
     id: "dwg2wt3is19kfw1e867",
@@ -72,17 +56,6 @@ List<Restaurant> testRestaurants = [
   ),
 ];
 
-List<Restaurant> testSearchRestaurants = [
-  Restaurant(
-    id: "dwg2wt3is19kfw1e867",
-    name: "Drinky Squash",
-    description:
-        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.",
-    pictureId: "18",
-    city: "Surabaya",
-    rating: 3.9,
-  ),
-];
 
 @GenerateMocks([ApiServiceTest])
 Future<void> main() async {
